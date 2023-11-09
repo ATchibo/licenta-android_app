d3e44a9494343569ffca6a181cad8f0d2f2e0091
Andrei Cibotariu <andreicibotariu@outlook.com>
2023-11-09 10:37:47 +0200

    login page design improvements

--- PlantBuddy/app/src/main/java/com/tchibo/plantbuddy/ui/pages/LoginPage.kt
+++ PlantBuddy/app/src/main/java/com/tchibo/plantbuddy/ui/pages/LoginPage.kt
@@ -1,22 +1,41 @@
 package com.tchibo.plantbuddy.ui.pages;
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable;
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.tchibo.plantbuddy.LocalNavController
+import com.tchibo.plantbuddy.R
 import com.tchibo.plantbuddy.utils.Routes
 import com.tchibo.plantbuddy.utils.ScreenInfo
 
@@ -25,26 +44,21 @@ import com.tchibo.plantbuddy.utils.ScreenInfo
 fun LoginPage(setCurrentScreenInfo: (ScreenInfo) -> Unit) {
     setCurrentScreenInfo(
         ScreenInfo(
-            title = "PlantBuddy"
+            title = stringResource(id = R.string.app_name)
         )
     )
 
     val navigator = LocalNavController.current
-    val openAlertDialog = remember { mutableStateOf(false) }
 
-    val (loginValue, setLoginValue) = remember {
-        mutableStateOf("")
-    }
-
-    val (pwValue, setPwValue) = remember {
-        mutableStateOf("")
-    }
+    var loginValue by remember { mutableStateOf("") }
+    var pwValue by remember { mutableStateOf("") }
+    var showErrorDialog by remember { mutableStateOf(false) }
 
     fun login() {
         if (loginValue == "test" && pwValue == "test")
             navigator.navigate(Routes.getNavigateHome())
         else {
-            openAlertDialog.value = true
+            showErrorDialog = true
         }
     }
 
@@ -52,25 +66,95 @@ fun LoginPage(setCurrentScreenInfo: (ScreenInfo) -> Unit) {
         navigator.navigate(Routes.getNavigateRegister())
     }
 
-    Column(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = "Login",
-            fontSize = 30.sp,
-            modifier = Modifier.padding(10.dp)
+    if (showErrorDialog) {
+        AlertDialog(
+            onDismissRequest = { showErrorDialog = false },
+            title = { Text(text = stringResource(id = R.string.error_title)) },
+            text = { Text(text = stringResource(id = R.string.error_message)) },
+            confirmButton = {
+                Button(onClick = { showErrorDialog = false }) {
+                    Text(text = stringResource(id = R.string.ok))
+                }
+            }
         )
+    }
 
-        TextField(value = loginValue, onValueChange = setLoginValue, label = {Text("Login")}, modifier = Modifier.padding(10.dp))
-        TextField(value = pwValue, onValueChange = setPwValue, label = {Text("Password")}, visualTransformation = PasswordVisualTransformation(), modifier = Modifier.padding(10.dp))
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = stringResource(id = R.string.login),
+                fontSize = 30.sp,
+                modifier = Modifier.padding(10.dp)
+            )
+            Spacer(modifier = Modifier.height(16.dp))
 
-        Spacer(modifier = Modifier.weight(1f))
-        Button(onClick = { register() }, modifier = Modifier.padding(10.dp)) {
-            Text(text = "Register")
-        }
-        Button(onClick = { login() }, modifier = Modifier.padding(10.dp)) {
-            Text(text = "Login")
+            TextField(
+                value = loginValue,
+                onValueChange = { loginValue = it },
+                label = { Text(text = stringResource(id = R.string.login)) },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+                    .clip(MaterialTheme.shapes.small)
+                    .background(MaterialTheme.colorScheme.surface)
+            )
+
+            TextField(
+                value = pwValue,
+                onValueChange = { pwValue = it },
+                label = { Text(text = stringResource(id = R.string.password)) },
+                visualTransformation = PasswordVisualTransformation(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+                    .clip(MaterialTheme.shapes.small)
+                    .background(MaterialTheme.colorScheme.surface)
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+                onClick = {login()},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+                    .clip(MaterialTheme.shapes.medium)
+            ) {
+                Text(text = stringResource(id = R.string.login))
+            }
+
+            val registerText = buildAnnotatedString {
+                withStyle(
+                    style = SpanStyle(
+                        color = MaterialTheme.colorScheme.onSurface,
+                        textDecoration = TextDecoration.None
+                    ),
+                ) {
+                    append("Don't have an account? ")
+                }
+                withStyle(
+                    style = SpanStyle(
+                        color = MaterialTheme.colorScheme.primary,
+                        textDecoration = TextDecoration.Underline
+                    )
+                ) {
+                    append("Register here")
+                }
+            }
+
+            ClickableText(
+                text = registerText,
+                onClick = {register()},
+                modifier = Modifier.padding(16.dp)
+            )
         }
     }
 }
