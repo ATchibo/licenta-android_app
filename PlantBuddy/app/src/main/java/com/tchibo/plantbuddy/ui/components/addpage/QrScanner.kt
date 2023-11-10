0610fb8efe18e78c7862133dd35b27f38848074e
Andrei Cibotariu <andreicibotariu@outlook.com>
2023-11-10 18:48:35 +0200

    some tiny improvements

--- PlantBuddy/app/src/main/java/com/tchibo/plantbuddy/ui/components/addpage/QrScanner.kt
+++ PlantBuddy/app/src/main/java/com/tchibo/plantbuddy/ui/components/addpage/QrScanner.kt
@@ -9,13 +9,21 @@ import androidx.camera.core.ImageCapture
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
@@ -25,49 +33,57 @@ import java.util.concurrent.Executors
 @Composable
 fun QrScanner() {
 
-    AndroidView({ context ->
-        val cameraExecutor = Executors.newSingleThreadExecutor()
-        val previewView = PreviewView(context).also {
-            it.scaleType = PreviewView.ScaleType.FILL_CENTER
-        }
-        val cameraProviderFuture = ProcessCameraProvider.getInstance(context)
-        cameraProviderFuture.addListener({
-            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+    val width = LocalConfiguration.current.screenWidthDp.dp
+    val height = width
 
-            val preview = Preview.Builder()
-                .build()
-                .also {
-                    it.setSurfaceProvider(previewView.surfaceProvider)
-                }
-
-            val imageCapture = ImageCapture.Builder().build()
-
-            val imageAnalyzer = ImageAnalysis.Builder()
-                .build()
-                .also {
-                    it.setAnalyzer(cameraExecutor, BarcodeAnalyser{ barcodeList ->
-                        Toast.makeText(context, barcodeList.get(0).displayValue, Toast.LENGTH_SHORT).show()
-                    })
-                }
-
-            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-
-            try {
-                // Unbind use cases before rebinding
-                cameraProvider.unbindAll()
-
-                // Bind use cases to camera
-                cameraProvider.bindToLifecycle(
-                    context as ComponentActivity, cameraSelector, preview, imageCapture, imageAnalyzer)
-
-            } catch(exc: Exception) {
-                Log.e("DEBUG", "Use case binding failed", exc)
+    Column(
+        modifier = Modifier
+            .size(width, height),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        AndroidView({ context ->
+            val cameraExecutor = Executors.newSingleThreadExecutor()
+            val previewView = PreviewView(context).also {
+                it.scaleType = PreviewView.ScaleType.FILL_CENTER
             }
-        }, ContextCompat.getMainExecutor(context))
-        previewView
-    },
-    modifier = Modifier.fillMaxWidth()
-        .height(150.dp)
-        .padding(10.dp)
-    )
+            val cameraProviderFuture = ProcessCameraProvider.getInstance(context)
+            cameraProviderFuture.addListener({
+                val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+                val preview = Preview.Builder()
+                    .build()
+                    .also {
+                        it.setSurfaceProvider(previewView.surfaceProvider)
+                    }
+
+                val imageCapture = ImageCapture.Builder().build()
+
+                val imageAnalyzer = ImageAnalysis.Builder()
+                    .build()
+                    .also {
+                        it.setAnalyzer(cameraExecutor, BarcodeAnalyser{ barcodeList ->
+                            Toast.makeText(context, barcodeList.get(0).displayValue, Toast.LENGTH_SHORT).show()
+                        })
+                    }
+
+                val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+                try {
+                    // Unbind use cases before rebinding
+                    cameraProvider.unbindAll()
+
+                    // Bind use cases to camera
+                    cameraProvider.bindToLifecycle(
+                        context as ComponentActivity, cameraSelector, preview, imageCapture, imageAnalyzer)
+
+                } catch(exc: Exception) {
+                    Log.e("DEBUG", "Use case binding failed", exc)
+                }
+            }, ContextCompat.getMainExecutor(context))
+            previewView
+        },
+            modifier = Modifier.size(width - 100.dp, height - 100.dp)
+        )
+    }
 }
