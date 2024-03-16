b248a82d1182da0356563247fa7437486031e908
Andrei Cibotariu <andreicibotariu@outlook.com>
2024-03-16 19:09:57 +0200

    added 2 new pages

---
+++ PlantBuddy/app/src/main/java/com/tchibo/plantbuddy/ui/pages/LogsPage.kt
@@ -0,0 +1,10 @@
+package com.tchibo.plantbuddy.ui.pages
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun LogsPage() {
+
+    Text(text = "Logs Page")
+}
---
+++ PlantBuddy/app/src/main/java/com/tchibo/plantbuddy/ui/pages/RaspberrySettingsPage.kt
@@ -0,0 +1,10 @@
+package com.tchibo.plantbuddy.ui.pages
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun RaspberrySettingsPage() {
+
+    Text(text = "Raspberry Settings Page")
+}
